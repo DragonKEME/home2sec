a648c3009198944c0432202a89d5cd711415a869
DragonKEME <_@_._>
2025-02-17 12:18:37 +0100

    config doc

--- composeApp/src/commonMain/kotlin/fr/insacvl/home2sec/Configuration.kt
+++ composeApp/src/commonMain/kotlin/fr/insacvl/home2sec/Configuration.kt
@@ -4,9 +4,8 @@ import fr.insacvl.home2sec.utils.DateUtils
 import io.ktor.client.HttpClient
 
 /**
- *
+ * Class to pass platform specific implementation to commonMain
  */
-
 data class Configuration (
     val httpClient: HttpClient,
     val dateUtils: DateUtils
