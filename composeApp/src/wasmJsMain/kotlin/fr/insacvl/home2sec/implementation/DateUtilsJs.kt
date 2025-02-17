83efec733f2a8c1ff853d7019d3c2ee28a28a23f
DragonKEME <_@_._>
2025-02-17 12:16:38 +0100

    update readme

--- composeApp/src/wasmJsMain/kotlin/fr/insacvl/home2sec/implementation/DateUtilsJs.kt
+++ composeApp/src/wasmJsMain/kotlin/fr/insacvl/home2sec/implementation/DateUtilsJs.kt
@@ -4,6 +4,7 @@ import fr.insacvl.home2sec.utils.DateUtils
 
 class DateUtilsJs: DateUtils {
     override fun FormatDate(isoDate: String): String {
+        // I found nothing to format date in kotlinJS :(
         return isoDate.replace('T',' ').substring(0..18)
     }
 
